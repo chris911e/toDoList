995bdffe9b384f213cb11de22f145ac268e21f93
Christopher Scheel <christopherscheel99@gmail.com>
2022-01-31 15:32:30 +0100

    Reactiveness implementiert

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,17 +1,27 @@
 let items = [];
+let counter = 0;
+
+function updateTaskCounter(){
+    counter++;
+    document.getElementById("counter").innerHTML = `Tasks erfüllt: ${counter}`
+}
 
 function addItem() {
     const input = document.getElementById("input")
-    items.push(input.value);
-    input.value="";
-    renderList();
+    if (input.value) {
+        items.push(input.value);
+        input.value="";
+        renderList();
+    } else {
+        alert("Do not let the field empty pls")
+    }
 }
 
 function renderList() {
     const list = document.getElementById("list")
     list.innerHTML = ""
     items.forEach(item => {
-        const newListItem = document.createElement("button")
+        const newListItem = document.createElement("li")
         newListItem.innerText = item
         list.append(newListItem)
     })
